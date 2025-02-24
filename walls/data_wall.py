e596830a10d315c652d91f355abd58728900bd14
harigro <trierbank@gmail.com>
2025-02-24 22:02:09 +0700

    menerapkan setter dan getter pada modul data_wall

--- walls/data_wall.py
+++ walls/data_wall.py
@@ -3,6 +3,16 @@ class DataWall:
         """Kelas untuk mengantur ketinggian dan posisi sumbu y rintangan agar selaras dengan lantai"""
         self.comb = comb
 
+    @property
+    def get_comb(self) -> tuple[int, int]:
+        """Mengambil nilai comb saat ini"""
+        return self._comb
+    
+    @get_comb.setter
+    def set_comb(self, new_comb: tuple[int, int]):
+        """Mengedit nilai comb saat ini"""
+        self._comb = new_comb
+
     def height_axis_y(self, stop: int, step: int) -> dict[int, int]:
         """Membuat diktonari dari tinggi dan posisi sumbu y"""
         tinggi = [t for t in range(self.comb[0], stop, step)]
