cf7dda6898e0a889c97254ce8f2b01d201af3f3d
harigro <trierbank@gmail.com>
2025-03-01 19:39:18 +0700

    menambah skor dan posisi fon pada modul settings

--- screen/settings.py
+++ screen/settings.py
@@ -1,11 +1,13 @@
 from collections import namedtuple
 
 # Namedtuple untuk konfigurasi
-Config = namedtuple("Config", ["box_size", "fps"])
+Config = namedtuple("Config", ["box_size", "fps", "skor", "posisi_fon"])
 
 # Konstanta
 BOX_SIZE = 50
 FPS = 60
+SKOR = 0
+POS_Fon = 115
 
 # Konfigurasi dalam namedtuple
-SETTINGS = Config(box_size=BOX_SIZE, fps=FPS)
+SETTINGS = Config(box_size=BOX_SIZE, fps=FPS, skor=SKOR, posisi_fon=POS_Fon)
