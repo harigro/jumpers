07a6b8068615dab032456d3ec6a4fb0a643c8ec3
harigro <trierbank@gmail.com>
2025-03-02 08:45:42 +0700

    memperbaiki typing metode getter dan setter modul wall

--- walls/wall.py
+++ walls/wall.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 class Object:
     def __init__(self, x: int, y: int, size: tuple[int, int]):
         """Inisialisasi objek dengan posisi, ukuran, dan atribut lainnya."""
@@ -6,22 +8,22 @@ class Object:
         self._osize = size
 
     @property
-    def get_ox(self) -> tuple[int, int]:
+    def get_ox(self) -> Union[int, int]:
         """Mengambil nilai ox saat ini"""
         return self._ox
     
     @get_ox.setter
-    def set_ox(self, new_ox: tuple[int, int]):
+    def set_ox(self, new_ox: Union[int, float]):
         """Mengedit nilai ox saat ini"""
         self._ox = new_ox
 
     @property
-    def get_oy(self) -> tuple[int, int]:
+    def get_oy(self) -> Union[int, int]:
         """Mengambil nilai oy saat ini"""
         return self._oy
     
     @get_oy.setter
-    def set_oy(self, new_oy: tuple[int, int]):
+    def set_oy(self, new_oy: Union[int, int]):
         """Mengedit nilai oy saat ini"""
         self._oy = new_oy
 
